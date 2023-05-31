6f70d943a5cad19e6ba7c3c455797e96951d43a4
jawad-ghouri <jawad.ghouri@devlodges.com>
2023-05-31 15:22:46 +0500

    added html structure and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// Importing various functional components
+import Wrapper from "./components/Wrapper/Wrapper";
+import Screen from "./components/Screen/Screen";
+import ButtonBox from "./components/ButtonBox/ButtonBox";
+import Button from "./components/Button/Button";
+import CalcProvider from "./context/CalcContext";
 
+// creating btnValues Array
+const btnValues = [
+  ["AC", "+/-", "%", "/"],
+  [7, 8, 9, "x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
+// Functional Component App
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CalcProvider>
+        <Wrapper>
+          <Screen />
+          <ButtonBox>
+            {btnValues.flat().map((btn, i) => (
+              <Button value={btn} key={i} />
+            ))}
+          </ButtonBox>
+        </Wrapper>
+      </CalcProvider>
     </div>
   );
 }
